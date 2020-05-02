07a2729abf3fc41e4b619826d9b7814d007def91
Luis Ordoñez <lporteniu@gmail.com>
2020-05-01 19:52:22 -0500

    Se agregaron directorios del dominio y prueba del comit comit

---
+++ Domain/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class City
+{
+	public City()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
