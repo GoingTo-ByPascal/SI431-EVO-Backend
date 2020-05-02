c65318d05372f517d4e6ac526321990312824e89
Claudia La serna <claudialaserna@MacBook-de-Claudia.local>
2020-05-02 03:00:00 -0500

    agregando city

--- Domain/Models/City.cs
+++ Domain/Models/City.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class City
+namespace GoingTo_API.Domain.Models
 {
-	public City()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+        public int Locatable { get; set; }
+    }
 }
