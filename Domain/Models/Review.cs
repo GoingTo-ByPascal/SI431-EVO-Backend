7fc684aa0802ca96b4300a56e6bc21ba90218d50
Shiohne <Shiohne@github.com>
2020-05-02 03:12:17 -0500

    Se añaden 2 tablas

---
+++ Domain/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoingTo_API.Domain.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public int UserId { get; set; }
+        public int PlaceId { get; set; }
+        public float Score { get; set; }
+        public int ImageId { get; set; }
+    }
+}
