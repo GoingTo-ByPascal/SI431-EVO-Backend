240163d5bdf0952c2046bb31bb7c4047fae7abbe
Claudia La serna <claudialaserna@MacBook-de-Claudia.local>
2020-05-02 03:16:37 -0500

    coreccion de la entidad city

--- Domain/Models/City.cs
+++ Domain/Models/City.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace GoingTo_API.Domain.Models
 {
-    public class Country
+    public class City
     {
         public int Id { get; set; }
         public string Name { get; set; }
