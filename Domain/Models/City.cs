3633e625784a4b7a92489857c3c5bab4160facd7
IPorteniu <43084237+IPorteniu@users.noreply.github.com>
2020-05-02 03:20:46 -0500

    Correción de City.cs
    Eliminé Locatable, porque aun no añadimos las interfaces.
    
    Porfavor Alonso, debes estar mas atento

--- Domain/Models/City.cs
+++ Domain/Models/City.cs
@@ -10,6 +10,5 @@ namespace GoingTo_API.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int CityId { get; set; }
-        public int Locatable { get; set; }
     }
 }
