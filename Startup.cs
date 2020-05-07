3035616632fb3dc67326c259e2067a6c472b20f9
IPorteniu <43084237+IPorteniu@users.noreply.github.com>
2020-05-07 03:26:56 -0500

    Revert "Solución a los problemas de mi vida"

--- Startup.cs
+++ Startup.cs
@@ -9,15 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using GoingTo_API.Domain.Persistence.Context;
-using GoingTo_API.Domain.Repositories;
-using GoingTo_API.Persistence;
-using GoingTo_API.Domain.Services;
-using GoingTo_API.Services;
-using GoingTo_API.Persistence.Repositories;
-using AutoMapper;
 
 namespace GoingTo_API
 {
@@ -34,21 +26,6 @@ namespace GoingTo_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<AppDbContext>(options =>
-            {
-                //options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"))
-                //options.UseInMemoryDatabase("goingto-on-memory");
-
-            });
-            services.AddScoped<ILocatableRepository, LocatableRepository>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            
-            services.AddScoped<ILocatableService, LocatableService>();
-            services.AddScoped<IUserService, UserService>();
-            
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            
-            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
