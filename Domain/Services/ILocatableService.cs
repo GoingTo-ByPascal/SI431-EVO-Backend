c4e6622f275469c1bfda2ae51b152275f2a173c1
IPorteniu <lporteniu@gmail.com>
2020-05-08 22:04:58 -0500

    Se completa feature de agregar a favoritos

--- Domain/Services/ILocatableService.cs
+++ Domain/Services/ILocatableService.cs
@@ -10,6 +10,7 @@ namespace GoingTo_API.Domain.Services
     public interface ILocatableService
     {
         Task<IEnumerable<Locatable>> ListAsync();
+        Task<IEnumerable<Locatable>> ListByUserIdAsync(int userId);
         Task<LocatableResponse> SaveAsync(Locatable locatable);
         Task<LocatableResponse> UpdateAsync(int id, Locatable locatable);
         Task<LocatableResponse> DeleteAsync(int id); 
