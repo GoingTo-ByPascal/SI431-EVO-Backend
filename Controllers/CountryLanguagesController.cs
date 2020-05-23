7f4acd0aa61f1e445272ef512f6d7a429b3cfe08
Shiohne <Shiohne@github.com>
2020-05-23 02:30:08 -0500

    Se añadió clase controllers vacías para Languages y CountryLanguages

---
+++ Controllers/CountryLanguagesController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using GoingTo_API.Domain.Services.Geographic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoingTo_API.Controllers
+{
+    public class CountryLanguagesController : Controller
+    {
+
+        private readonly IMapper _mapper;
+        private readonly ICountryLanguageService _countryLanguageService;
+
+        public CountryLanguagesController(ICountryLanguageService countryLanguageService, IMapper mapper)
+        {
+            _mapper = mapper;
+            _countryLanguageService = countryLanguageService;
+        }
+    }
+}
---
+++ Controllers/LanguagesController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using GoingTo_API.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoingTo_API.Controllers
+{
+    public class LanguagesController: Controller
+    {
+        private readonly ILanguageService _languageService;
+        private readonly IMapper _mapper;
+
+        public LanguagesController(ILanguageService languageService, IMapper mapper)
+        {
+            _languageService = languageService;
+            _mapper = mapper;
+        }
+    }
+}
