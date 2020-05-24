cb9ec929c61f2c17bb71d2b5ec16ef2be135c4b1
Shiohne <Shiohne@github.com>
2020-05-24 12:23:29 -0500

    UserReviewsController constructor

---
+++ Controllers/UserReviewsController.cs
@@ -0,0 +1,25 @@
+﻿using GoingTo_API.Domain.Repositories;
+using GoingTo_API.Domain.Services.Interactions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoingTo_API.Controllers
+{
+    [Route("/api/users/{userId}/reviews")]
+    [Produces("application/json")]
+    public class UserReviewsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IReviewService _reviewService;
+
+        public UserReviewsController(IReviewService reviewService, IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+            _reviewService = reviewService;
+        }
+
+    }
+}
