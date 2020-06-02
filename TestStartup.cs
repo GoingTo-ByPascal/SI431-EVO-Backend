bd9ff58ae4d507720da8914608e0008d48581be2
Shiohne <Shiohne@github.com>
2020-06-01 22:59:57 -0500

    TestStartup created

---
+++ TestStartup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using GoingTo_API.Domain.Persistence.Context;
+using GoingTo_API.Domain.Repositories;
+using GoingTo_API.Persistence;
+using GoingTo_API.Domain.Services;
+using GoingTo_API.Services;
+using GoingTo_API.Persistence.Repositories;
+using Microsoft.OpenApi.Models;
+using AutoMapper;
+using System.IO;
+using System.Reflection;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace GoingTo_API
+{
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            // Do not add custom services here
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GoingTo API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
