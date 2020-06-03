fa00f82004c0dec5b6da3ab104b06de8e911b7bb
Jaime Alonso Garrido Guevara <alonso.garrido.2901@hotmail.com>
2020-06-02 23:57:58 -0500

    actualizacon
    Conflicts:
    	Controllers/LocatableTipsController.cs

--- Resources/SaveTipResource.cs
+++ Resources/SaveTipResource.cs
@@ -7,7 +7,11 @@ namespace GoingTo_API.Resources
     public class SaveTipResource
     {
         [Required]
+<<<<<<< HEAD
         [MaxLength(140)]
+=======
+        [MaxLength(45)]
+>>>>>>> 4fe95f5... Fix en resources 2
         public string Text { get; set; }
     }
 }
