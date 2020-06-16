9493c3b1a9720daff342e4518ec2a928d78a9c2e
IPorteniu <lporteniu@gmail.com>
2020-06-15 20:43:26 -0500

    Cualquier error culpen al gato

--- GoingTo-Test/UnitTest1.cs
+++ GoingTo-Test/UnitTest1.cs
@@ -1,5 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RestSharp;
 
 namespace GoingTo_Test
 {
@@ -9,13 +8,6 @@ namespace GoingTo_Test
         [TestMethod]
         public void TestMethod1()
         {
-            
-            var client = new RestClient("https://goingto.azurewebsites.net/api");
-
-            var request = new RestRequest("/categories/{categoryId}",Method.GET);
-            request.AddUrlSegment("categoryId", 1);
-
-            var content = client.Execute(request).Content;
         }
     }
 }
