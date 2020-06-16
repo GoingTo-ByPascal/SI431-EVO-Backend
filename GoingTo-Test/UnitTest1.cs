1804bcd79423dce1284fcac12be3a6a70131624e
IPorteniu <lporteniu@gmail.com>
2020-06-16 00:49:41 -0500

    Unit testing de prueba

--- GoingTo-Test/UnitTest1.cs
+++ GoingTo-Test/UnitTest1.cs
@@ -1,4 +1,10 @@
+using GoingTo_API.Domain.Models.Geographic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System.Collections.Generic;
+using Utf8Json;
 
 namespace GoingTo_Test
 {
@@ -8,6 +14,31 @@ namespace GoingTo_Test
         [TestMethod]
         public void TestMethod1()
         {
+            var client = new RestClient("https://goingto.azurewebsites.net/api");
+
+            var request = new RestRequest("/categories", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Category { Name = "a la categoria"});
+
+            var response = client.Execute<Category>(request);
+
+            //var desearilize = new JsonDeserializer();
+            //var output = desearilize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["name"];
+
+
+            Assert.AreEqual(response.Data.Name, "a la categoria","No es correcto");
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var client = new RestClient("https://goingto.azurewebsites.net/api");
+
+            var request = new RestRequest("/categories/{categoryid}", Method.GET);
+            request.AddUrlSegment("categoryId", 1);
+
+            var response = client.Execute<Category>(request).Content;
         }
     }
 }
